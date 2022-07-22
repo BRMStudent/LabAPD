38fdba9c8afeb5c4275aed067ff9d363c7ab68a3
Baramee <102453964+BRMStudent@users.noreply.github.com>
2022-07-22 08:38:46 +0700

    Fix Code LabTest

--- LabTest/Form1.cs
+++ LabTest/Form1.cs
@@ -12,7 +12,7 @@ namespace LabTest {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-            this.ControlBox = false;
+            this.MaximizeBox = false;
         }
 
         private void Form1_Load(object sender, EventArgs e) {
