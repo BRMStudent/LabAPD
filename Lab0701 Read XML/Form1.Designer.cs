113fe2c8efc85c82885b15e844f2dd7422e22663
Baramee <63011212041@o365.msu.ac.th>
2022-09-21 20:10:33 +0700

    add lab0701

---
+++ Lab0701 Read XML/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Lab0701_Read_XML {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.buttonLINQ = new System.Windows.Forms.Button();
+            this.buttonReadXML = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(312, 12);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(90, 37);
+            this.buttonFind.TabIndex = 0;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // buttonLINQ
+            // 
+            this.buttonLINQ.Location = new System.Drawing.Point(312, 55);
+            this.buttonLINQ.Name = "buttonLINQ";
+            this.buttonLINQ.Size = new System.Drawing.Size(90, 37);
+            this.buttonLINQ.TabIndex = 1;
+            this.buttonLINQ.Text = "LINQ";
+            this.buttonLINQ.UseVisualStyleBackColor = true;
+            this.buttonLINQ.Click += new System.EventHandler(this.buttonLINQ_Click);
+            // 
+            // buttonReadXML
+            // 
+            this.buttonReadXML.Location = new System.Drawing.Point(12, 12);
+            this.buttonReadXML.Name = "buttonReadXML";
+            this.buttonReadXML.Size = new System.Drawing.Size(90, 37);
+            this.buttonReadXML.TabIndex = 2;
+            this.buttonReadXML.Text = "ReadXML";
+            this.buttonReadXML.UseVisualStyleBackColor = true;
+            this.buttonReadXML.Click += new System.EventHandler(this.buttonReadXML_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 110);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 468);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 590);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonReadXML);
+            this.Controls.Add(this.buttonLINQ);
+            this.Controls.Add(this.buttonFind);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Read XML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Button buttonLINQ;
+        private System.Windows.Forms.Button buttonReadXML;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
